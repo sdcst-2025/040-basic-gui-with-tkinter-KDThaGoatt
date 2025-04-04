d45f603bcbf835f29f718baf76737d3ba61c3222
KDThaGoatt <kyand906@deltalearns.ca>
2025-04-04 10:36:40 -0700

    a

--- task3.py
+++ task3.py
@@ -8,8 +8,11 @@ window.geometry("257x141")
 
 dogphoto = PhotoImage(file="dog.png")
 label1 = tk.Label(window, image=dogphoto)
-label1.grid(row=0, column=10)
 label2 = tk.Label(window, text="Pochacco!")
 label3 = tk.Label(window, text="A cuddly little puppy! This is from the same\ncreators who brought you Keropi and Kero Kero", background="#aaffff")
+
+label1.grid(row=0, column=0, padx=0, pady=0, sticky=tk.E)
+label2.grid(row=0, column=1, padx=0, pady=0,sticky=tk.W)
+label3.grid(row=2, column=0, columnspan=2, padx=0, pady=5)
 
 window.mainloop()
